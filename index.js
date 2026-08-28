f581bdfa8fc199864539cb2192b8c6bcf164af66
XiaoPi <107348054+xiaopi992@users.noreply.github.com>
2026-08-28 16:20:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname)));
 
 // Route utama
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // Jalankan server
