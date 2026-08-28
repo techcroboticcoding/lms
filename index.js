a4d311a9d09b1823cdf1a4bf52568781003448cd
XiaoPi <107348054+xiaopi992@users.noreply.github.com>
2026-08-28 16:18:01 +0700

    add express

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Port dari hosting atau 3000 untuk lokal
+const PORT = process.env.PORT || 3000;
+
+// Folder public sebagai static files
+app.use(express.static(path.join(__dirname)));
+
+// Route utama
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Jalankan server
+app.listen(PORT, () => {
+    console.log(`Server berjalan di port ${PORT}`);
+});
